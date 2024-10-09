41d45aa6b3e14c857f27a7bab3fe3725386681e6
Daniil <wearver@yandex.ru>
2024-10-09 17:21:05 +0400

    service, bin

--- service_create.cmd
+++ service_create.cmd
@@ -1,12 +1,38 @@
-set ARGS=--wf-l3=ipv4,ipv6 --wf-tcp=80,443 --dpi-desync=fake,split --dpi-desync-ttl=7 --dpi-desync-fooling=md5sig
-call :srvinst winws1
-rem set ARGS=--wf-l3=ipv4,ipv6 --wf-udp=443 --dpi-desync=fake 
-rem call :srvinst winws2
-goto :eof
+@echo off
+chcp 65001 >nul
+:: 65001 - UTF-8
 
-:srvinst
-net stop %1
-sc delete %1
-sc create %1 binPath= "\"%~dp0winws.exe\" %ARGS%" DisplayName= "zapret DPI bypass : %1" start= auto
-sc description %1 "zapret DPI bypass software"
-sc start %1
+:: Path check
+set scriptPath=%~dp0
+set "path_no_spaces=%scriptPath: =%"
+if not "%scriptPath%"=="%path_no_spaces%" (
+    echo Путь содержит пробелы. 
+    echo Пожалуйста, переместите скрипт в директорию без пробелов.
+    pause
+    exit /b
+)
+
+:: Admin rights check
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Скрипт запущен без прав администратора. 
+    echo Запустите от имени администратора.
+    pause
+    exit /b
+)
+
+set BIN=%~dp0bin\
+set ARGS=--wf-tcp=443 --wf-udp=443,50000-65535 ^
+--filter-udp=443 --hostlist=\"%~dp0list-discord.txt\" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
+--filter-udp=50000-65535 --dpi-desync=fake,tamper --dpi-desync-any-protocol --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
+--filter-tcp=443 --hostlist=\"%~dp0list-discord.txt\" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=\"%BIN%tls_clienthello_www_google_com.bin\"
+
+set SRVCNAME=zapret
+
+net stop "%SRVCNAME%"
+sc delete "%SRVCNAME%"
+sc create "%SRVCNAME%" binPath= "%BIN%winws.exe %ARGS%" DisplayName= "zapret DPI bypass : winws1" start= auto
+sc description "%SRVCNAME%" "zapret DPI bypass software"
+sc start "%SRVCNAME%"
+
+pause
